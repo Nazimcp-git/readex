ab57827e570a2a344cd7fc15c19e5e7a81b176bb
Nazimcp-git <78524092+Nazimcp-git@users.noreply.github.com>
2024-11-16 19:15:03 +0530

    Update script.js

--- legal/js/script.js
+++ legal/js/script.js
@@ -1,6 +1,6 @@
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
-
+const Links = document.querySelector('.links');
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
@@ -9,3 +9,11 @@ toggleButton.addEventListener('click', () => {
 document.getElementById('buynow').addEventListener('click', function() {
     window.location.href = '/product.html';
 });
+
+function close1() {
+    Links.classList.add("close-animation");
+    setTimeout(()=>{
+        navbarLinks.classList.remove('active');
+        Links.classList.remove("close-animation");
+    },400);
+}
